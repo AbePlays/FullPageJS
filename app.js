ead85043890e977172dc996ff6f7a7c047b8dff5
Abe <abhi.rawat456@gmail.com>
2019-11-05 18:09:58 +0530

    fixing page 3 css

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ new fullpage("#fullpage", {
     const tl = new TimelineMax({ delay: 0.5 });
     tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
     if (destination.index === 1) {
-      const chairs = document.querySelectorAll(".sekiro");
+      const chairs = document.querySelectorAll(".rdr");
       const description = document.querySelector(".description");
-      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-70%" }).fromTo(
-        description,
-        0.5,
-        { opacity: 0, y: "50" },
-        { y: "0", opacity: 1 }
-      );
+      const logo = document.querySelector(".image");
+      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-70%" })
+        .fromTo(
+          description,
+          0.5,
+          { opacity: 0, y: "50" },
+          { y: "0", opacity: 1 }
+        )
+        .fromTo(logo, 0.7, { x: "-100%" }, { x: "5%" });
     }
   }
 });
