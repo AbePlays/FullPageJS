d174f31ff32f07672f6b0e4a1ea0517f4af34dd5
Abe <abhi.rawat456@gmail.com>
2019-11-05 19:21:55 +0530

    Fixing page 2

--- app.js
+++ app.js
@@ -5,19 +5,12 @@ new fullpage("#fullpage", {
     const section = destination.item;
     const title = section.querySelector("h1");
     const tl = new TimelineMax({ delay: 0.5 });
-    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
-    if (destination.index === 1) {
-      const chairs = document.querySelectorAll(".rdr");
-      const description = document.querySelector(".description");
-      const logo = document.querySelector(".image");
-      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-70%" })
-        .fromTo(
-          description,
-          0.5,
-          { opacity: 0, y: "50" },
-          { y: "0", opacity: 1 }
-        )
-        .fromTo(logo, 0.7, { x: "-100%" }, { x: "5%" });
-    }
+    const met = document.querySelector(".met");
+    tl.fromTo(
+      title,
+      0.5,
+      { y: "50", opacity: 0 },
+      { y: "0", opacity: 1 }
+    ).fromTo(met, 0.5, { x: "-110%" }, { x: "10%" });
   }
 });
