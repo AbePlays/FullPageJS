22af078a0b6ba52d37db34e2c2c0e2eec125e117
Abe <abhi.rawat456@gmail.com>
2019-11-06 07:19:52 +0530

    completed page 3

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ new fullpage("#fullpage", {
     const title = section.querySelector("h1");
     const tl = new TimelineMax({ delay: 0.5 });
     const met = document.querySelector(".met");
-    tl.fromTo(
-      title,
-      0.5,
-      { y: "50", opacity: 0 },
-      { y: "0", opacity: 1 }
-    ).fromTo(met, 0.5, { x: "-110%" }, { x: "10%" });
+    const gow = document.querySelector(".gow");
+    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 })
+      .fromTo(met, 0.5, { x: "-110%" }, { x: "10%" })
+      .fromTo(gow, 0.5, { x: "120%" }, { x: "-1%" });
   }
 });
