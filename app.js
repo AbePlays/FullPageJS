4e1d1b627292938038e965732e63209130cce243
Abe <abhi.rawat456@gmail.com>
2019-11-06 07:27:58 +0530

    fixing animation

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ new fullpage("#fullpage", {
     const tl = new TimelineMax({ delay: 0.5 });
     const met = document.querySelector(".met");
     const gow = document.querySelector(".gow");
-    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 })
-      .fromTo(met, 0.5, { x: "-110%" }, { x: "10%" })
-      .fromTo(gow, 0.5, { x: "120%" }, { x: "-1%" });
+    if (destination.index === 0) {
+      tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
+    } else if (destination.index === 1) {
+      tl.fromTo(
+        title,
+        0.5,
+        { y: "50", opacity: 0 },
+        { y: "0", opacity: 1 }
+      ).fromTo(met, 0.5, { x: "-110%" }, { x: "10%" });
+    } else if (destination.index === 2) {
+      tl.fromTo(
+        title,
+        0.5,
+        { y: "50", opacity: 0 },
+        { y: "0", opacity: 1 }
+      ).fromTo(gow, 0.5, { x: "120%" }, { x: "-1%" });
+    }
   }
 });
